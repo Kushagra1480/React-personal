ef45676257bd45e86ce0aa69a8f5d6bc582e3812
Kushagra Kartik <kushagrakartik1480@gmail.com>
2023-01-04 11:48:01 +0400

    added modal

--- src/App.js
+++ src/App.js
@@ -11,18 +11,19 @@ import { MantineProvider,
   Group, 
   Box, 
   Space, 
-  Title, 
-  List, 
+  Title,  
   Card,
   Checkbox, 
-  Text } from '@mantine/core';
+  Text,
+  Modal } from '@mantine/core';
 
-import {Trash, Pencil} from 'tabler-icons-react'
+import {Trash, Pencil, Plus, Check} from 'tabler-icons-react'
 
 function App() {
   const [todoList, setTodoList] = useState([])
   const [input, setInput] = useState("")
   const [editingTodo, setEditingTodo] = useState(null)
+  const [opened, setOpened] = useState(false)
 
   return (
     <MantineProvider 
@@ -31,23 +32,34 @@ function App() {
         fontFamily: 'Roboto',
         spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
         }} withGlobalStyles withNormalizeCSS>
-      <Container size = 'xs'>
-        <Box size = 'xs' mx = 'auto'> 
-          <Title align='center'>TODO LIST</Title>
-          <form onSubmit = {(event) => handleClick(event, setTodoList, setInput)}>
-            <TextInput  
-              type = 'text'
-              label = 'Enter Todo'
-              size = 'sm'
-              name = 'todo' 
-              onInput={(e) => setInput(e.target.value)} 
-              value = {input}
-            />
-            <Space h = 'sm'/>
-            <Group position = 'right' mt = 'mid'>
-              <Button type = "submit">Add Todo</Button>
-            </Group>
-          </form>
+      <Container size = 'xs' spacing = 'md' position = 'apart'>
+        <Box size = 'xs' mx = 'auto' spacing = 'md' > 
+          <Title align='center'>ToDo List</Title>
+            <Button leftIcon = {<Plus />} onClick = {() => setOpened(true)}>New Task</Button>
+            <Modal
+              opened = {opened}
+              title = 'New Task'
+              withCloseButton = {false}
+              onClose = {() => {setOpened(false)}}
+              centered
+            >
+              <form onSubmit = {(event) => {handleClick(event, setTodoList, setInput)
+              setOpened(false)}}>
+                <TextInput  
+                  type = 'text'
+                  label = 'Enter Todo'
+                  size = 'sm'
+                  name = 'todo' 
+                  onInput={(e) => setInput(e.target.value)} 
+                  value = {input}
+                />
+                <Space h = 'sm'/>
+                <Group position = 'right' mt = 'mid'>
+                  <Button onClick = {() => {setOpened(false)}}>Cancel</Button>
+                  <Button leftIcon = {<Plus />} type = "submit" >Add Todo</Button>
+                </Group>
+              </form>
+            </Modal>
           <Box>
               {todoList.map((todo, index) => {
                 return(
@@ -55,30 +67,33 @@ function App() {
                     <Card 
                     withBorder
                     mt={'md'}
-                    style = {{
-                      textDecoration : todo.done ? 'line-through' : 'none', 
-                      color : todo.done ? 'grey' : 'black',
-                      }}
                     key = {index}  
                       >
                       {editingTodo && editingTodo.key === todo.key ? 
                       (
                         <>
-                          <input 
-                            type = "text" 
-                            value = {editingTodo.task} 
-                            onChange = {(event) => 
-                              setEditingTodo({...editingTodo, task: event.target.value})}
-                          />
-                          <button onClick={() => handleSave(index, 
-                            todoList, 
-                            editingTodo,
-                            setTodoList, 
-                            setEditingTodo)}>Save</button>
+                          <Group position = 'apart'>
+                            <TextInput
+                              type = "text" 
+                              value = {editingTodo.task} 
+                              onChange = {(event) => 
+                                setEditingTodo({...editingTodo, task: event.target.value})}
+                            />
+                            <Button 
+                              onClick={() => handleSave(index, 
+                                todoList, 
+                                editingTodo,
+                                setTodoList, 
+                                setEditingTodo)}
+                              leftIcon = {<Check />}
+                              >Save</Button>
+                          </Group>
                         </>
                       ) : (
                         <>
                           <Checkbox
+                            radius = 'xl'
+                            checked = {todo.done}
                             onChange={(event) => 
                               toggleCompleted(index, todoList, setTodoList, event.target.checked)}
                             label = {<Text size='lg' style = {{
@@ -87,10 +102,16 @@ function App() {
                               }}>{todo.task}</Text>}
                             />
                           <Group position = 'apart' spacing='xl' mt={'md'}>
-                            <Button leftIcon = {<Trash />} onClick = {() => handleDelete(index, todoList, setTodoList)}>
+                            <Button 
+                              color = 'red' 
+                              leftIcon = {<Trash />} 
+                              onClick = {() => handleDelete(index, todoList, setTodoList)}>
                               Delete
                             </Button>
-                            <Button leftIcon = {<Pencil />} onClick = {() => handleEdit(index, todoList, setEditingTodo)}>
+                            <Button 
+                              color = 'cyan' 
+                              leftIcon = {<Pencil />} 
+                              onClick = {() => handleEdit(index, todoList, setEditingTodo)}>
                               Edit
                             </Button>
                           </Group>
